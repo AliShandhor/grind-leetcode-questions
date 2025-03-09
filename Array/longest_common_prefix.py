fae113db1b8596b7f444057b871edb71cdaf0f12
Ali Shandhor <a.shanzoor@gmail.com>
2025-03-08 20:12:25 -0500

    updating longest_common_prefix.py

--- Array/longest_common_prefix.py
+++ Array/longest_common_prefix.py
@@ -1,18 +1,18 @@
-
-
 from typing import List
+
 class Solution:
     def longestCommonPrefix(self, words: List[str]) -> str:
+        # Handle the case where the input list is empty
+        if not words:
+            return ""
         
-        counter = 0 
-        common_prefix = []
-            
-    
-        for word in words:
-            for char in word:
-              common_prefix.append()
-                
-                print('')
-       
-        return ""
+        common_prefix = ""
         
+        for i in range(len(words[0])):
+            char = words[0][i]
+            for word in words:
+                if i >= len(word) or word[i] != char:
+                    return common_prefix
+            common_prefix += char
+        
+        return common_prefix
