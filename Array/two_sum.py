bd9cab3d4da5abfa297082e6d35e80221a338cbc
Ali Shandhor <a.shanzoor@gmail.com>
2025-03-10 12:00:56 -0400

    update two_sum.py

--- Array/two_sum.py
+++ Array/two_sum.py
@@ -1,8 +1,8 @@
 def two_sum(nums, target):
     num_map = {}  # Dictionary to store number and its index
-    for i, num in enumerate(nums):
-        complement = target - num
+    for i in range(len(nums)):  # Using a regular for loop
+        complement = target - nums[i]
         if complement in num_map:
             return [num_map[complement], i]  # Return indices of the two numbers
-        num_map[num] = i  # Store the index of the current number
+        num_map[nums[i]] = i  # Store the index of the current number
     return []  # Return empty list if no solution
